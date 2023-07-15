81390e77a53fce3a98618f489b7aed1de5c30128
Val Grace <gracevalerie1@gmail.com>
2023-07-15 14:45:55 +0300

    feat: add airtime topup

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ var cors = require("cors")
 var indexRouter = require('./routes/index');
 var tokenRouter = require('./routes/confirmToken');
 var mpesaRouter = require('./routes/mpesaapi')
-
+var airtimeRouter = require('./routes/airtime')
 // var bodyParser = require('body-parser')
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(cors({
 }))
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(bodyParser.urlencoded({ extended: false}))
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/v2/api/details', indexRouter);
 app.use('/v2/api/confirmUrl', tokenRouter);
 app.use('/mpesa', mpesaRouter)
+app.use('/airtime', airtimeRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'))
+  
   getDetails(res)
 });
 
